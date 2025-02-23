33afa8715ed7b24922c47f1ee7331d8269833425
José Iván García Martínez <jgarciam@ia.com.mx>
2025-02-23 02:38:24 -0600

    update game model to entity

--- app/src/main/java/com/example/gameslist/data/models/Game.kt
+++ app/src/main/java/com/example/gameslist/data/models/Game.kt
@@ -1,21 +1,15 @@
 package com.example.gameslist.data.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "game")
 data class Game(
+    @PrimaryKey(autoGenerate = false)
     val id: Int,
     val title: String,
     val thumbnail: String,
     @SerializedName("short_description")
     val shortDescription: String,
-    val gameUrl: String,
-    val genre: String,
-    val platform: String,
-    val publisher: String,
-    val developer: String,
-    @SerializedName("release_date")
-    val releaseDate: String,
-    @SerializedName("freetogame_profile_url")
-    val freetogameProfileUrl: String,
-    val description: String,
 )
