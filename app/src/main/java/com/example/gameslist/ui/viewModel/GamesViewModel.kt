41e4dbd61f952c1113bdefa6e4ce01af2f1492cc
José Iván García Martínez <jgarciam@ia.com.mx>
2025-02-23 03:07:55 -0600

    Set the db repository to the current viewmodel

--- app/src/main/java/com/example/gameslist/ui/viewModel/GamesViewModel.kt
+++ app/src/main/java/com/example/gameslist/ui/viewModel/GamesViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.gameslist.data.models.Game
 import com.example.gameslist.domain.GetGames
+import com.example.gameslist.repository.local.LocalGameRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,9 +14,13 @@ import javax.inject.Inject
 @HiltViewModel
 class GamesViewModel @Inject constructor(
     private val getGamesD: GetGames,
+    private val localGameRepository: LocalGameRepository,
 ) : ViewModel() {
     private val _games = MutableLiveData<List<Game>>()
     val games: LiveData<List<Game>> get() = _games
+
+    private val _game = MutableLiveData<Game>()
+    val game: LiveData<Game> get() = _game
 
     init {
         getGames()
@@ -28,6 +33,26 @@ class GamesViewModel @Inject constructor(
                 _games.value = games
             } catch (_: Exception) {
             }
+        }
+    }
+
+    private fun getAllDBGames() {
+        viewModelScope.launch {
+            _games.value = localGameRepository.getAllGames()
+        }
+    }
+
+    fun getGameById(gameId: Int) {
+        viewModelScope.launch {
+            val game = localGameRepository.getGameById(gameId)
+            _game.value = game
+        }
+    }
+
+    fun deleteGameById(id: Int) {
+        viewModelScope.launch {
+            localGameRepository.deleteGameById(id)
+            getAllDBGames()
         }
     }
 }
