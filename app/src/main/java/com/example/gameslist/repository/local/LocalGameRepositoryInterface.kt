cfa3f3fae2f9786a10d4d8f7ff29c58630257f67
José Iván García Martínez <jgarciam@ia.com.mx>
2025-02-23 02:50:08 -0600

    create the BD repository to provide the data to the UI

---
+++ app/src/main/java/com/example/gameslist/repository/local/LocalGameRepositoryInterface.kt
@@ -0,0 +1,10 @@
+package com.example.gameslist.repository.local
+
+import com.example.gameslist.data.models.Game
+
+interface LocalGameRepositoryInterface {
+    suspend fun insertAllGames(games: List<Game>)
+    suspend fun getAllGames(): List<Game?>
+    suspend fun getGameById(gameId: Int): Game?
+    suspend fun deleteGameById(gameId: Int)
+}
