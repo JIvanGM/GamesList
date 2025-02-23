83cab4671c7254456feab1c99594840c5fa373f3
José Iván García Martínez <jgarciam@ia.com.mx>
2025-02-23 06:44:17 -0600

    Add search bat in HomeScreen to find games by the user input or by the history search.

--- app/src/main/java/com/example/gameslist/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/gameslist/ui/screens/HomeScreen.kt
@@ -1,51 +1,61 @@
 package com.example.gameslist.ui.screens
 
-import android.annotation.SuppressLint
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.example.gameslist.data.models.Game
@@ -53,33 +63,103 @@ import com.example.gameslist.ui.navigation.Screens
 import com.example.gameslist.ui.viewModel.GamesViewModel
 import kotlinx.coroutines.launch
 
-@RequiresApi(Build.VERSION_CODES.N)
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(gamesViewModel: GamesViewModel, navController: NavController) {
     val games = gamesViewModel.games.observeAsState(listOf()).value
+    var expanded by remember { mutableStateOf(false) }
+    var query by remember { mutableStateOf("") }
+    var historyItems = remember { mutableStateListOf("aion") }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "Games App") }
-            )
-        }
-    ) { innerPadding ->
+    Scaffold { innerPadding ->
         val listState = rememberLazyListState()
         val scope = rememberCoroutineScope()
-        LazyColumn(
-            state = listState,
+
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(paddingValues = innerPadding)
         ) {
-            val grouped = games.groupBy { it.title[0] }
-            grouped.forEach { initial, games ->
-                stickyHeader {
-                    CharacterHeader(char = initial, Modifier.fillParentMaxWidth())
+            SearchBar(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                inputField = {
+                    SearchBarDefaults.InputField(
+                        query = query,
+                        onQueryChange = {
+                            query = it
+                        },
+                        onSearch = {
+                            if (query.isNotEmpty()) {
+                                gamesViewModel.searchGame(query)
+                                historyItems.add(query)
+                            } else {
+                                gamesViewModel.getAllDBGames()
+                            }
+                            expanded = false
+                        },
+                        expanded = expanded,
+                        onExpandedChange = { expanded = it },
+                        placeholder = { Text("Buscar") },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Search,
+                                contentDescription = "search icon"
+                            )
+                        },
+                        trailingIcon = {
+                            if (expanded) {
+                                Icon(
+                                    Icons.Default.Close,
+                                    modifier = Modifier.clickable {
+                                        if (query.isNotEmpty()) {
+                                            query = ""
+                                        } else {
+                                            expanded = false
+                                        }
+                                        gamesViewModel.getAllDBGames()
+                                    },
+                                    contentDescription = "close icon"
+                                )
+                            }
+                        },
+                    )
+                },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+            ) {
+                historyItems.forEach {
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(all = 14.dp)
+                        .clickable {
+                            query = it
+                            gamesViewModel.searchGame(query)
+                            expanded = false
+                        }
+                    ) {
+                        Icon(
+                            Icons.Default.History,
+                            modifier = Modifier.padding(end = 10.dp),
+                            contentDescription = "History icon"
+                        )
+                        Text(text = it)
+                    }
                 }
+            }
+
+            LazyColumn(
+                state = listState,
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 16.dp
+                ),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
                 items(games) { game ->
                     GameCard(
                         game = game,
@@ -135,7 +215,7 @@ fun GameCard(game: Game, navController: NavController, gamesViewModel: GamesView
                     .padding(10.dp)
                     .align(Alignment.CenterVertically)
             ) {
-                Text(text = game.title, fontWeight = FontWeight.Bold)
+                Text(text = game.title + " - " + game.genre, fontWeight = FontWeight.Bold)
                 Text(text = game.shortDescription, maxLines = 2, overflow = TextOverflow.Ellipsis)
             }
             IconButton(
@@ -143,6 +223,7 @@ fun GameCard(game: Game, navController: NavController, gamesViewModel: GamesView
                     gamesViewModel.deleteGameById(game.id)
                 },
                 modifier = Modifier
+                    .align(Alignment.CenterVertically)
                     .weight(.3f)
                     .padding(8.dp)
                     .background(Color.Red, shape = CircleShape)
@@ -157,21 +238,6 @@ fun GameCard(game: Game, navController: NavController, gamesViewModel: GamesView
     }
 }
 
-@Composable
-private fun CharacterHeader(char: Char, modifier: Modifier) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .padding(start = 12.dp, top = 6.dp, bottom = 6.dp)
-    ) {
-        Text(
-            text = char.toString(),
-            style = TextStyle(color = MaterialTheme.colorScheme.primary, fontSize = 20.sp),
-            fontWeight = FontWeight.Bold,
-        )
-    }
-}
 
 @Composable
 fun ScrollToTopButton(onClick: () -> Unit) {
@@ -187,7 +253,7 @@ fun ScrollToTopButton(onClick: () -> Unit) {
                 .clip(shape = CircleShape)
                 .size(65.dp),
         ) {
-            Icon(Icons.Filled.KeyboardArrowUp, "arrow up")
+            Icon(Icons.Default.KeyboardArrowUp, "arrow up")
         }
     }
 }
